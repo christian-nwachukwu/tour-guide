fd8a29b5833e8278a4363415c49a3043fdebfdbb
christian nwachukwu <christian.nwachukwu2020@gmail.com>
2023-01-03 00:59:37 +0100

    Creating navigation to various pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
