09074b39b43b43aa0cdeceda3ec86f10b09dcc29
christian nwachukwu <christian.nwachukwu2020@gmail.com>
2023-01-06 06:39:40 +0100

    completion of first-des and first-des-reverse

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -2,33 +2,45 @@ import Dubai from "../assets/dubai.jpg";
 import Effiel from "../assets/effiel.jpg";
 import Moscow from "../assets/moscow.jpg";
 import Rome from "../assets/rome.jpg";
+import DestinationData from "./DestinationData";
 import "./Destination.css";
+
 
 const Destination = () => {
     return(
         <div className="destination">
-        <h1>Travel Destinations</h1>
-        <p>You have the opportunity
+            <h1>Travel Destinations</h1>
+            <p>You have the opportunity
              to see popular travel sites.</p>
-        <div className="first-des">
-            <div className="desc-text">
-                <h2>Destination Name, Location</h2>
-                <p>Components can be passed as props,
-                    which stands for properties.
-                    Props are like function arguments, 
-                    and you send them into the component 
-                    as attributes.
-                    You will learn more about props in 
-                    the next chapter.
-                </p>
-            </div>
 
-            <div className="image">
-                <img alt="" src={Moscow}/>
-                <img alt="" src={Rome}/>
-            </div>
+            <DestinationData
+                className="first-des"
+                heading="Destination Name, Location"
+                text="Components can be passed as props,
+                which stands for properties.
+                Props are like function arguments, 
+                and you send them into the component 
+                as attributes.
+                You will learn more about props in 
+                the next chapter."
+                img1={Dubai}
+                img2={Moscow}
+            />
+
+            <DestinationData
+                className="first-des-reverse"
+                heading="2nd Destination Name, Location"
+                text="Components can be passed as props,
+                which stands for properties.
+                Props are like function arguments, 
+                and you send them into the component 
+                as attributes.
+                You will learn more about props in 
+                the next chapter."
+                img1={Effiel}
+                img2={Rome}
+            />
         </div>
-    </div>
     );
 }
 
--- src/components/DestinationData.js
+++ src/components/DestinationData.js
@@ -1,10 +1,26 @@
+import Dubai from "../assets/dubai.jpg";
+import Effiel from "../assets/effiel.jpg";
+import Moscow from "../assets/moscow.jpg";
+import Rome from "../assets/rome.jpg";
 import { Component } from "react";
 import "./Destination.css";
 
-class Destination extends Component{
+class DestinationData extends Component{
     render(){
         return(
-            <div></div>
+            <div className={this.props.className}>
+                <div className="desc-text">
+                    <h2>{this.props.heading}</h2>
+                    <p>{this.props.text}</p>
+                </div>
+
+                <div className="image">
+                    <img alt="img" src={this.props.img1}/>
+                    <img alt="img" src={this.props.img2}/>
+                </div>
+            </div>
         )
     }
 }
+
+export default DestinationData;
