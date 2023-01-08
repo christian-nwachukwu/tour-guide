cdffcdee7f20416c96af230ab84260f16a46fa3c
christian nwachukwu <christian.nwachukwu2020@gmail.com>
2023-01-08 23:34:00 +0100

    Creation of Trip.js, css and TripData

---
+++ src/components/Trip.js
@@ -0,0 +1,13 @@
+import "./Trip.css";
+
+function Trip() {
+    return(
+        <div className="trip">
+            <h1>Recent Trips</h1>
+                <p>You can discover unique places
+                    or destinations using Google maps</p>
+        </div>
+    );
+}
+
+export default Trip;
